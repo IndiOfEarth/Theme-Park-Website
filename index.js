628c0d07a7a261ea587dc7c2de32a1f977200ebd
Indi Caburian <indi.caburian@outlook.com>
2024-05-28 18:02:26 +0100

    Updated featured, cards and contacts- in progress

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-// <script></script>
-
+let swiper = new Swiper(".container", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    grabCursor: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    }
+})
